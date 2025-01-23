eb891ea6c4835bb7ec0eb9419c87d98c15a87fae
greyced <cedric.duperron@gmail.com>
2025-01-23 21:39:53 +0100

    init angular project

---
+++ src/features/dashboard/dashboard-filter/dashboard-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-filter',
+  imports: [],
+  templateUrl: './dashboard-filter.component.html',
+  styleUrl: './dashboard-filter.component.scss'
+})
+export class DashboardFilterComponent {
+
+}
